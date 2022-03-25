84024b9ef29a53b989deca02b18ee44da9117dca
DmitryPonyn <102251712+DmitryPonyn@users.noreply.github.com>
2022-03-25 22:57:15 +0700

    Add files via upload

---
+++ WPDP_Flash_Constructor/WPDP_Flash_Constructor/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WPDP_Flash_Constructor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Name = "label2";
+            // 
+            // Form2
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::WPDP_Flash_Constructor.Properties.Resources.wallpaper;
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.Opacity = 0.85D;
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WPDP_Flash_Constructor/WPDP_Flash_Constructor/Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace WPDP_Flash_Constructor
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c .\\tools\\bootice.exe /diskinfo /list: list disks /file=.\\tools\\1.txt",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+            process.WaitForExit();
+            string[] lines = File.ReadAllLines(@".\tools\1.txt");
+            foreach (string line in lines)
+                if (line.Contains("DRIVE"))
+                    comboBox1.Items.Add(line);
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                Process process2 = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c .\\tools\\USBPREP.exe DRIVE=" + comboBox1.SelectedIndex + " clean & .\\tools\\USBPREP.exe DRIVE=" + comboBox1.SelectedIndex + " VISTA FORCELBA " +
+                    "& .\\tools\\USBPREP.exe DRIVE=" + comboBox1.SelectedIndex + " volume=WPDP_USB & .\\tools\\BOOTICE.exe /DEVICE=" + comboBox1.SelectedIndex +
+                    ":0 /partitions /activate /assign_letter=W",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                });
+                process2.WaitForExit();
+                MessageBox.Show("Этап завершён./Stage complete.");
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = "/c taskkill /im WPDP_Flash_Constructor.exe & taskkill /im WPDP_Flash_Constructor.exe",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+                Process p2 = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    Arguments = "/c .\\tools\\7z.exe x osnova.7z -oW:\\",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                });
+                p2.WaitForExit();
+                MessageBox.Show("Операция завершена./Operation has complete");
+        }            
+    }
+}
